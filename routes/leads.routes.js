bbfb8429119243ffae577b70c2c1cb42f7b06297
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-04-07 19:10:46 -0600

    commit

--- routes/leads.routes.js
+++ routes/leads.routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const leadsController = require('../controllers/leads.controller');
 
+// Ruta para mostrar la página de leads
 router.get('/leads', leadsController.getLeadsPage);
-
 module.exports = router;
