d0b9015dc63a926942b1e84c391a9c8baf79cfe7
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-04-09 09:58:57 -0600

    Pruebas para eliminar lead

--- pruebas/CU17EL.test.js
+++ pruebas/CU17EL.test.js
@@ -32,6 +32,20 @@ describe('Pruebas para Eliminar Lead', () => {
         expect(rowsAfterDeletion.length).toBe(0);
     });
 
+    test('Lead a eliminar no existe', async () => {
+        const IDUsuario = 400;
+        const telefonoEliminado = '552-7734';
+        const [rowsBeforeDeletion] = await Leads.fetchOne_lead(IDUsuario, telefonoEliminado);
+        console.log(rowsBeforeDeletion);
+        expect(rowsBeforeDeletion.length).toBe(0);
+        
+        await Leads.fetchOne_delete_lead(IDUsuario,telefonoEliminado);
+
+        const [rowsAfterDeletion] = await Leads.fetchOne_lead(IDUsuario, telefonoEliminado);
+        console.log(rowsAfterDeletion);
+        expect(rowsAfterDeletion.length).toBe(0);
+    });
+
     test('cancela la eliminación', async () => {
         const IDUsuario = 60;
         const [rowsBeforeDeletion] = await Leads.fetchOne_lead(IDUsuario);
@@ -44,20 +58,29 @@ describe('Pruebas para Eliminar Lead', () => {
 
     test('error durante la eliminación', async () => {
         const IDUsuario = 55;
-        const telefonoEliminado = 'telefono_inexistente';
+        const telefonoEliminado = '552-6789';
+        // Usamos mock y spy para simular un error
+        // Con mock creamos una versión simulada para forzar un error
+        // spyOn rastrea el comportamiento de la funcion, si arroja error, devuelve el mensaje de error
+
+        // Mockear fetchOne_delete_lead para simular un error
+        jest.spyOn(Leads, 'fetchOne_delete_lead').mockImplementation(() => {
+            throw new Error('Error simulado: No se pudo eliminar el registro');
+        });
+
         const [rowsBeforeDeletion] = await Leads.fetchOne_lead(IDUsuario);
         console.log(rowsBeforeDeletion);
         expect(rowsBeforeDeletion.length).toBe(1);
 
-        // Simular un error durante la eliminacion
         try {
-            await Leads.fetchOne_delete_lead(IDUsuario,telefonoEliminado);
+            await Leads.fetchOne_delete_lead(IDUsuario, telefonoEliminado);
         } catch (error) {
-            console.log(error);
+            console.log(error.message);
         }
 
         const [rowsAfterError] = await Leads.fetchOne_lead(IDUsuario);
         console.log(rowsAfterError);
         expect(rowsAfterError.length).toBe(1);
+        expect(Leads.fetchOne_delete_lead).toHaveBeenCalledWith(IDUsuario, telefonoEliminado);
     });
 });
