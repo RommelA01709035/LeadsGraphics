0e880a9ea9ad2f71be657c82ff6afa212345a458
Ian Julian Estrada Castro <ianjulianestradacastro@MacBook-Air-de-Ian-3.local>
2024-04-15 15:25:23 -0600

    Middleware para proteger rutas

--- routes/leads.routes.js
+++ routes/leads.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+const isAuth = require('../util/is-auth');
 const leadsController = require('../controllers/leads.controller');
 
-router.get('/leads', leadsController.getLeadsPage);
+router.get('/leads', isAuth, leadsController.getLeadsPage);
 
 module.exports = router;
--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const isAuth = require('../util/is-auth');
 const usuarioController = require('../controllers/usuario.controller');
 
-router.get('/usuarios', usuarioController.getUsuarioPage);
-router.post('/reactivate-usuario', usuarioController.post_reactivate_Usuario);
-router.post('/delete-usuario', usuarioController.post_delete_Usuario);
+router.get('/usuarios', isAuth, usuarioController.getUsuarioPage);
+router.post('/reactivate-usuario', isAuth, usuarioController.post_reactivate_Usuario);
+router.post('/delete-usuario', isAuth, usuarioController.post_delete_Usuario);
 
 
 module.exports = router;
