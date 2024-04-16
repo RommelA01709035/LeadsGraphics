4844043664bc531a2b88604023e74821f9d8208d
RommelA01709035 <93753971+RommelA01709035@users.noreply.github.com>
2024-04-15 23:40:03 -0600

    Se aniade prueba para registrarse

--- util/database.js
+++ util/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'leadgraphs',
-    password: 'JulianSQL'
+    password: 'AguaUwu2'
 });
 
 module.exports = pool.promise();
