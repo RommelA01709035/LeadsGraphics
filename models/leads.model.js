fdcff33cc8c79c33bd8bec36b31bbcc9ad8db56e
Ángel Francisco García Guzmán <angelgarciag14@gmail.com>
2024-04-16 12:56:33 -0600

    arreglar error en base de datos

--- models/leads.model.js
+++ models/leads.model.js
@@ -39,15 +39,6 @@ module.exports = class Leads {
         return db.execute('SELECT * FROM leads WHERE IDLead = ?', [id]);
     }
 
-    static fetch(id) {
-        if(id) {
-            return this.fetchOne(id);
-        } else {
-            return this.fetchAll();
-        return db.execute('SELECT * FROM leads WHERE IDLead = ?', [id]);
-        }
-    }
-
     static async buscarPorNombre(nombre) {
         try {
             const query = `
@@ -157,52 +148,6 @@ module.exports = class Leads {
             throw error;
         }
     }
+    
 
-    static async buscarPorNombre(nombre) {
-        try {
-            const query = `
-            SELECT *
-            FROM leads
-            WHERE LOWER(nombre) LIKE LOWER(?)`;
-    
-            const [rows, fields] = await pool.query(query, [`%${nombre}%`]);
-    
-            console.log('Datos de leads encontrados:', rows);
-    
-            return rows;
-        } catch (error) {
-            throw error;
-        }
-    }
-
-    // Método para guardar un nuevo lead en la base de datos
-    async save() {
-        const values = Object.values(this).map(val => (val === undefined ? null : val));
-    
-        // Mapear los nombres de las columnas y unirlos en una cadena separada por comas
-        const columns = Object.keys(this).join(', ');
-    
-        // Crear una cadena de marcadores de posición para los valores
-        const placeholders = values.map(() => '?').join(', ');
-    
-        const result = await db.execute(
-            `INSERT INTO leads 
-            (IDHistorial, IDWorkspace, Telefono, Nombre, Valor, Ganado, Correo, Etiqueta, Compania, Fecha_Primer_Mensaje, Hora_Primer_Mensaje, Primer_Mensaje, Fecha_Ultimo_Mensaje, Hora_Ultimo_Mensaje, Ultimo_Mensaje, Estado_Lead, Seller_Asignado, Embudo, Etapa, Archivado, Creado_Manualmente) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-            [
-                this.IDHistorial, this.IDWorkspace, this.Telefono, this.Nombre, this.Valor, this.Ganado, this.Correo, 
-                this.Etiqueta, this.Compania, this.Fecha_Primer_Mensaje, this.Hora_Primer_Mensaje, this.Primer_Mensaje, 
-                this.Fecha_Ultimo_Mensaje, this.Hora_Ultimo_Mensaje, this.Ultimo_Mensaje, this.Estado_Lead, 
-                this.Seller_Asignado, this.Embudo, this.Etapa, this.Archivado, this.Creado_Manualmente
-            ]
-        );
-
-        // Devuelve el resultado de la inserción
-        return result[0];
-    }
-
-    static deleteLead(id){
-        return db.execute('CALL deleteLead(?)',[id]);
-    };
-    
 }
